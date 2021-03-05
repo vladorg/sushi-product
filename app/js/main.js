237188233650a217014d6a3ab591c6491284a5cf
vladorg <klayss4@gmail.com>
2021-03-05 16:52:26 +0200

    header

--- app/js/main.js
+++ app/js/main.js
@@ -1,27 +1,176 @@
 window.onload = () => {
 
-  // HEADER  
-  const catalog_btn = document.querySelectorAll('.catalog__btn'),
-        catalog = document.querySelector('.catalog'),
-        header = document.querySelector('.header'),
-        open_wish = document.querySelectorAll('.open_wish'),
-        open_cart = document.querySelectorAll('.open_cart'),
-        wishlist = document.querySelector('.wishlist'),
-        cart = document.querySelector('.cart'),
-        modals = document.querySelectorAll('.modal'),
-        open_auth = document.querySelectorAll('.open_auth'),
-        modal_auth = document.querySelector('.modalAuth'),
-        open_reg = document.querySelectorAll('.open_register'),
-        modal_reg = document.querySelector('.modalRegister'),
-        open_forgot = document.querySelectorAll('.open_forgot'),
-        modal_forgot= document.querySelector('.modalForgot'),
-        open_check = document.querySelectorAll('.open_check'),
-        modal_check= document.querySelector('.modalCheck'),
-        open_reset = document.querySelectorAll('.open_reset'),
-        modal_reset= document.querySelector('.modalReset'),
-        open_change_success = document.querySelectorAll('.open_change_success'),
-        modal_change_success= document.querySelector('.modalSuccessPass'),
-        modal_close = document.querySelectorAll('.modal__close');
+  let getElem = (selector, single = true) => single ? document.querySelector(selector) : document.querySelectorAll(selector);
+
+
+  // BASE ================================================
+
+  const modal_close = getElem('.modal__close', false),
+    modals = {
+      root: getElem('.modal', false),
+      auth: {
+        opener: getElem('.open_auth', false),
+        item: getElem('.modalAuth')
+      },
+      register: {
+        opener: getElem('.open_register', false),
+        item: getElem('.modalRegister')
+      },
+      forgot: {
+        opener: getElem('.open_forgot', false),
+        item: getElem('.modalForgot')
+      },
+      check: {
+        opener: getElem('.open_check', false),
+        item: getElem('.modalCheck')
+      },
+      reset: {
+        opener: getElem('.open_reset', false),
+        item: getElem('.modalReset')
+      },
+      change_success: {
+        opener: getElem('.open_change_success', false),
+        item: getElem('.modalSuccessPass')
+      },
+      ask: {
+        opener: getElem('.open_ask', false),
+        item: getElem('.modalAsk')
+      },
+      review: {
+        opener: getElem('.open_review', false),
+        item: getElem('.modalReview')
+      },
+      callback: {
+        opener: getElem('.open_callback', false),
+        item: getElem('.modalCallback')
+      }
+    }
+
+  // open modals
+  openModals(modals.auth.opener, modals.auth.item, 'auth'); // auth
+  openModals(modals.register.opener, modals.register.item, 'register'); // register
+  openModals(modals.forgot.opener, modals.forgot.item, 'forgot'); // forgot
+  openModals(modals.check.opener, modals.check.item, 'check'); // check
+  openModals(modals.reset.opener, modals.reset.item, 'reset'); // reset
+  openModals(modals.change_success.opener, modals.change_success.item, 'change success'); // change success
+  openModals(modals.ask.opener, modals.ask.item, 'ask'); // ask
+  openModals(modals.review.opener, modals.review.item, 'review'); // review
+  openModals(modals.callback.opener, modals.callback.item, 'callback'); // callback
+
+  function openModals(elem, modal, name) {
+    if (modal) {
+      for (let i = 0; i < elem.length; i++) {
+        elem[i].addEventListener('click', e => {
+          e.preventDefault();
+          closeModals(); // close other modals before open target modal
+          modal.classList.add('modal--open');
+          document.body.classList.add('no-scroll');
+          setTimeout(() => {
+            document.body.classList.add('hasActiveModal');
+          }, 200);
+        });
+      }
+    } else {
+      return console.log(`modal ${name} is undefined`);
+    }
+
+  }
+
+  // close all modals
+  for (let i = 0; i < modal_close.length; i++) {
+    modal_close[i].addEventListener('click', closeModals);
+  }
+
+  function closeModals() {
+    for (let i = 0; i < modals.root.length; i++) {
+      modals.root[i].classList.remove('modal--open');
+    }
+    document.body.classList.remove('no-scroll');
+    document.body.classList.remove('hasActiveModal');
+  }
+
+  // close modal when click outside them
+  document.addEventListener('click', e => {
+    if (document.body.classList.contains('hasActiveModal')) {
+      for (let i = 0; i < modals.root.length; i++) {
+        if (modals.root[i].classList.contains('modal--open')) {
+          let current_modal = modals.root[i].querySelector('.modal__win');
+          if (!current_modal.contains(e.target)) {
+            modals.root[i].classList.remove('modal--open');
+            document.body.classList.remove('no-scroll');
+            document.body.classList.remove('hasActiveModal');
+          }
+        }
+      }
+    }
+  });
+
+
+
+  // main slider
+  const swiper = new Swiper('.banner__slider', {
+    slidesPerView: 1,
+    slidesPerColumn: 1,
+    progressbarOpposite: true,
+    loop: true,
+    effect: 'slide',
+    autoplay: {
+      delay: 4000,
+    },
+    autoHeight: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<div class="dot swiper-pagination-bullet"><div class="progressCircle"><div class="progressCircle__inner"><div class="progressCircle__mask full"><div class="fill"></div></div><div class="progressCircle__mask half"><div class="fill"></div></div><div class="progressCircle__inside"></div></div></div></div>';
+      },
+    },
+  });
+
+
+  // function on customize slides change effect
+  swiper.on('slideChangeTransitionStart', function () {
+    let slide_text = getElem('.banner .swiper-slide-active .banner__title').innerText;
+    let slide_btn_text = getElem('.banner .swiper-slide-active .banner__link').innerText;
+    let slide_btn_href = getElem('.banner .swiper-slide-active .banner__link').getAttribute('href');
+    function getSlideLink(){
+      return getElem('.banner .swiper-slide-active .banner__link').getAttribute('href');
+    }
+    //console.log(setTimeout(getSlideLink, 500));
+    
+    let caption = getElem('.banner__captions .banner__caption');
+
+    caption.querySelector('.banner__title').classList.remove('banner__title--anim');    
+    caption.querySelector('.banner__link').classList.remove('banner__link--anim');
+    setTimeout(() => {
+      caption.querySelector('.banner__title').innerHTML = slide_text;
+      caption.querySelector('.banner__link').innerHTML = slide_btn_text;
+      caption.querySelector('.banner__link').setAttribute('href', slide_btn_href);
+    }, 200);
+    setTimeout(() => {
+      caption.querySelector('.banner__title').classList.add('banner__title--anim');
+      caption.querySelector('.banner__link').classList.add('banner__link--anim');
+    }, 250);
+  });
+
+
+
+
+
+  // HEADER  ================================================
+  const catalog_btn = getElem('.catalog__btn', false),
+    catalog = getElem('.catalog'),
+    header = getElem('.header'),
+    open_wish = getElem('.open_wish', false),
+    open_cart = getElem('.open_cart', false),
+    wishlist_wrap = getElem('.header__tool--wish'),
+    wishlist = getElem('.wishlist'),
+    cart_wrap = getElem('.header__tool--cart'),
+    cart = getElem('.cart'),
+    logged = getElem('.logged'),
+    logged_wrap = getElem('.header__tool--logged'),
+    open_logged = getElem('.open_logged', false);
 
   // open catalog 
   if (catalog_btn) {
@@ -49,116 +198,85 @@ window.onload = () => {
     // fix header
     let header_height = header.clientHeight;
     window.addEventListener('scroll', () => {
-      if (this.pageYOffset > header_height + 100) {
+      if (this.pageYOffset > header_height) {
         header.classList.add('header--fix');
-      } else {
+      } else if (this.pageYOffset == 0) {
         header.classList.remove('header--fix');
       }
     });
 
-    // open wish and cart
+    // open cart
     for (let i = 0; i < open_cart.length; i++) {
-      open_cart[i].addEventListener('click', (e) => {
-        wishlist.classList.remove('headerDropdown--open');        
-        setTimeout(() => {
-          cart.classList.toggle('headerDropdown--open');
-        }, 200);
+      open_cart[i].addEventListener('click', function (e) {
+        if (window.innerWidth > 768) {
+          e.preventDefault();
+          wishlist.classList.remove('headerDropdown--open');
+          logged.classList.remove('headerDropdown--open');
+          setTimeout(() => {
+            cart.classList.toggle('headerDropdown--open');
+          }, 200);
+        } else {
+          let link = this.getAttribute('data-link');
+          if (link) {
+            window.location = link;
+          } else {
+            console.log('cart link is not set or set incorrect');
+          }
+        }
       });
     }
+
+    // open wish
     for (let i = 0; i < open_wish.length; i++) {
-      open_wish[i].addEventListener('click', (e) => {
+      open_wish[i].addEventListener('click', e => {
         cart.classList.remove('headerDropdown--open');
+        logged.classList.remove('headerDropdown--open');
         setTimeout(() => {
           wishlist.classList.toggle('headerDropdown--open');
         }, 200);
       });
     }
-    cart.addEventListener('click', (e) => {
-      e.stopPropagation();
-    });
-    wishlist.addEventListener('click', (e) => {
-      e.stopPropagation();
-    });
 
-
-
-    // open auth modal
-    for (let i = 0; i < open_auth.length; i++) {
-      open_auth[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        modal_auth.classList.add('modal--open');
-        document.body.classList.add('no-scroll');
+    // open logged
+    for (let i = 0; i < open_logged.length; i++) {
+      open_logged[i].addEventListener('click', e => {
+        cart.classList.remove('headerDropdown--open');
+        wishlist.classList.remove('headerDropdown--open');
+        setTimeout(() => {
+          open_logged[i].classList.toggle('open_logged--opened');
+          logged.classList.toggle('headerDropdown--open');
+        }, 200);
       });
     }
 
-    // open auth register
-    for (let i = 0; i < open_reg.length; i++) {
-      open_reg[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        modal_auth.classList.remove('modal--open');
-        modal_reg.classList.add('modal--open');
-        document.body.classList.add('no-scroll');
-      });
-    }
+    cart.addEventListener('click', e => e.stopPropagation());
+    wishlist.addEventListener('click', e => e.stopPropagation());
+    logged.addEventListener('click', e => e.stopPropagation());
 
-    // open modal forgot
-    for (let i = 0; i < open_forgot.length; i++) {
-      open_forgot[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        modal_auth.classList.remove('modal--open');
-        modal_forgot.classList.add('modal--open');
-        document.body.classList.add('no-scroll');
-      });
-    }
 
-    // open modal check
-    for (let i = 0; i < open_check.length; i++) {
-      open_check[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        modal_forgot.classList.remove('modal--open');
-        modal_check.classList.add('modal--open');
-        document.body.classList.add('no-scroll');
-      });
-    }
-
-    // open modal reset
-    for (let i = 0; i < open_reset.length; i++) {
-      open_reset[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        modal_check.classList.remove('modal--open');
-        modal_reset.classList.add('modal--open');
-        document.body.classList.add('no-scroll');
-      });
-    }
-
-    // open modal change pass success
-    for (let i = 0; i < open_change_success.length; i++) {
-      open_change_success[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        modal_reset.classList.remove('modal--open');
-        modal_change_success.classList.add('modal--open');
-        document.body.classList.add('no-scroll');
-
-        // setTimeout(() => {
-        //   modal_change_success.classList.remove('modal--open');
-        // }, 3000);
-      });
-    }
-
-    // close all modals
-    for (let i = 0; i < modal_close.length; i++) {
-      modal_close[i].addEventListener('click', (e) => {
-        for (let i=0;i<modals.length;i++) {
-          modals[i].classList.remove('modal--open');
+    document.addEventListener('click', e => {
+      if (!cart_wrap.contains(e.target)) {
+        cart.classList.remove('headerDropdown--open');
+      }
+      if (!wishlist_wrap.contains(e.target)) {
+        wishlist.classList.remove('headerDropdown--open');
+      }
+      if (!logged_wrap.contains(e.target)) {
+        logged.classList.remove('headerDropdown--open');
+        for (let i=0;i<open_logged.length;i++) {
+          open_logged[i].classList.remove('open_logged--opened');        
         }
-        document.body.classList.remove('no-scroll');
-      });
-    }
-    
+      }
+    });
+
 
   }
 
-  
+
+
+
+
+
 
 
 
--- dist/js/main.js
+++ dist/js/main.js
@@ -3,32 +3,135 @@
 var _this = void 0;
 
 window.onload = function () {
-  // HEADER  
-  var catalog_btn = document.querySelectorAll('.catalog__btn'),
-      catalog = document.querySelector('.catalog'),
-      header = document.querySelector('.header'),
-      open_wish = document.querySelectorAll('.open_wish'),
-      open_cart = document.querySelectorAll('.open_cart'),
-      wishlist = document.querySelector('.wishlist'),
-      cart = document.querySelector('.cart'),
-      modals = document.querySelectorAll('.modal'),
-      open_auth = document.querySelectorAll('.open_auth'),
-      modal_auth = document.querySelector('.modalAuth'),
-      open_reg = document.querySelectorAll('.open_register'),
-      modal_reg = document.querySelector('.modalRegister'),
-      open_forgot = document.querySelectorAll('.open_forgot'),
-      modal_forgot = document.querySelector('.modalForgot'),
-      open_check = document.querySelectorAll('.open_check'),
-      modal_check = document.querySelector('.modalCheck'),
-      open_reset = document.querySelectorAll('.open_reset'),
-      modal_reset = document.querySelector('.modalReset'),
-      open_change_success = document.querySelectorAll('.open_change_success'),
-      modal_change_success = document.querySelector('.modalSuccessPass'),
-      modal_close = document.querySelectorAll('.modal__close'); // open catalog 
+  var getElem = function getElem(selector) {
+    var single = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
+    return single ? document.querySelector(selector) : document.querySelectorAll(selector);
+  }; // BASE ================================================
+
+
+  var modal_close = getElem('.modal__close', false),
+      modals = {
+    root: getElem('.modal', false),
+    auth: {
+      opener: getElem('.open_auth', false),
+      item: getElem('.modalAuth')
+    },
+    register: {
+      opener: getElem('.open_register', false),
+      item: getElem('.modalRegister')
+    },
+    forgot: {
+      opener: getElem('.open_forgot', false),
+      item: getElem('.modalForgot')
+    },
+    check: {
+      opener: getElem('.open_check', false),
+      item: getElem('.modalCheck')
+    },
+    reset: {
+      opener: getElem('.open_reset', false),
+      item: getElem('.modalReset')
+    },
+    change_success: {
+      opener: getElem('.open_change_success', false),
+      item: getElem('.modalSuccessPass')
+    },
+    ask: {
+      opener: getElem('.open_ask', false),
+      item: getElem('.modalAsk')
+    },
+    review: {
+      opener: getElem('.open_review', false),
+      item: getElem('.modalReview')
+    },
+    callback: {
+      opener: getElem('.open_callback', false),
+      item: getElem('.modalCallback')
+    }
+  }; // open modals
+
+  openModals(modals.auth.opener, modals.auth.item, 'auth'); // auth
+
+  openModals(modals.register.opener, modals.register.item, 'register'); // register
+
+  openModals(modals.forgot.opener, modals.forgot.item, 'forgot'); // forgot
+
+  openModals(modals.check.opener, modals.check.item, 'check'); // check
+
+  openModals(modals.reset.opener, modals.reset.item, 'reset'); // reset
+
+  openModals(modals.change_success.opener, modals.change_success.item, 'change success'); // change success
+
+  openModals(modals.ask.opener, modals.ask.item, 'ask'); // ask
+
+  openModals(modals.review.opener, modals.review.item, 'review'); // review
+
+  openModals(modals.callback.opener, modals.callback.item, 'callback'); // callback
+
+  function openModals(elem, modal, name) {
+    if (modal) {
+      for (var i = 0; i < elem.length; i++) {
+        elem[i].addEventListener('click', function (e) {
+          e.preventDefault();
+          closeModals(); // close other modals before open target modal
+
+          modal.classList.add('modal--open');
+          document.body.classList.add('no-scroll');
+          setTimeout(function () {
+            document.body.classList.add('hasActiveModal');
+          }, 200);
+        });
+      }
+    } else {
+      return console.log("modal ".concat(name, " is undefined"));
+    }
+  } // close all modals
+
+
+  for (var i = 0; i < modal_close.length; i++) {
+    modal_close[i].addEventListener('click', closeModals);
+  }
+
+  function closeModals() {
+    for (var _i = 0; _i < modals.root.length; _i++) {
+      modals.root[_i].classList.remove('modal--open');
+    }
+
+    document.body.classList.remove('no-scroll');
+    document.body.classList.remove('hasActiveModal');
+  } // close modal when click outside them
+
+
+  document.addEventListener('click', function (e) {
+    if (document.body.classList.contains('hasActiveModal')) {
+      for (var _i2 = 0; _i2 < modals.root.length; _i2++) {
+        if (modals.root[_i2].classList.contains('modal--open')) {
+          var current_modal = modals.root[_i2].querySelector('.modal__win');
+
+          if (!current_modal.contains(e.target)) {
+            modals.root[_i2].classList.remove('modal--open');
+
+            document.body.classList.remove('no-scroll');
+            document.body.classList.remove('hasActiveModal');
+          }
+        }
+      }
+    }
+  }); // HEADER  ================================================
+
+  var catalog_btn = getElem('.catalog__btn', false),
+      catalog = getElem('.catalog'),
+      header = getElem('.header'),
+      open_wish = getElem('.open_wish', false),
+      open_cart = getElem('.open_cart', false),
+      wishlist_wrap = getElem('.header__tool--wish'),
+      wishlist = getElem('.wishlist'),
+      cart_wrap = getElem('.header__tool--cart'),
+      cart = getElem('.cart'); // open catalog 
 
   if (catalog_btn) {
-    for (var i = 0; i < catalog_btn.length; i++) {
-      catalog_btn[i].addEventListener('click', function () {
+    for (var _i3 = 0; _i3 < catalog_btn.length; _i3++) {
+      catalog_btn[_i3].addEventListener('click', function () {
         var _this2 = this;
 
         catalog.classList.toggle('catalog--open');
@@ -53,24 +156,36 @@ window.onload = function () {
     // fix header
     var header_height = header.clientHeight;
     window.addEventListener('scroll', function () {
-      if (_this.pageYOffset > header_height + 100) {
+      if (_this.pageYOffset > header_height) {
         header.classList.add('header--fix');
-      } else {
+      } else if (_this.pageYOffset == 0) {
         header.classList.remove('header--fix');
       }
-    }); // open wish and cart
+    }); // open cart
 
-    for (var _i = 0; _i < open_cart.length; _i++) {
-      open_cart[_i].addEventListener('click', function (e) {
-        wishlist.classList.remove('headerDropdown--open');
-        setTimeout(function () {
-          cart.classList.toggle('headerDropdown--open');
-        }, 200);
+    for (var _i4 = 0; _i4 < open_cart.length; _i4++) {
+      open_cart[_i4].addEventListener('click', function (e) {
+        if (window.innerWidth > 768) {
+          e.preventDefault();
+          wishlist.classList.remove('headerDropdown--open');
+          setTimeout(function () {
+            cart.classList.toggle('headerDropdown--open');
+          }, 200);
+        } else {
+          var link = this.getAttribute('data-link');
+
+          if (link) {
+            window.location = link;
+          } else {
+            console.log('cart link is not set or set incorrect');
+          }
+        }
       });
-    }
+    } // open wish
 
-    for (var _i2 = 0; _i2 < open_wish.length; _i2++) {
-      open_wish[_i2].addEventListener('click', function (e) {
+
+    for (var _i5 = 0; _i5 < open_wish.length; _i5++) {
+      open_wish[_i5].addEventListener('click', function (e) {
         cart.classList.remove('headerDropdown--open');
         setTimeout(function () {
           wishlist.classList.toggle('headerDropdown--open');
@@ -79,81 +194,19 @@ window.onload = function () {
     }
 
     cart.addEventListener('click', function (e) {
-      e.stopPropagation();
+      return e.stopPropagation();
     });
     wishlist.addEventListener('click', function (e) {
-      e.stopPropagation();
-    }); // open auth modal
+      return e.stopPropagation();
+    });
+    document.addEventListener('click', function (e) {
+      if (!cart_wrap.contains(e.target)) {
+        cart.classList.remove('headerDropdown--open');
+      }
 
-    for (var _i3 = 0; _i3 < open_auth.length; _i3++) {
-      open_auth[_i3].addEventListener('click', function (e) {
-        e.preventDefault();
-        modal_auth.classList.add('modal--open');
-        document.body.classList.add('no-scroll');
-      });
-    } // open auth register
-
-
-    for (var _i4 = 0; _i4 < open_reg.length; _i4++) {
-      open_reg[_i4].addEventListener('click', function (e) {
-        e.preventDefault();
-        modal_auth.classList.remove('modal--open');
-        modal_reg.classList.add('modal--open');
-        document.body.classList.add('no-scroll');
-      });
-    } // open modal forgot
-
-
-    for (var _i5 = 0; _i5 < open_forgot.length; _i5++) {
-      open_forgot[_i5].addEventListener('click', function (e) {
-        e.preventDefault();
-        modal_auth.classList.remove('modal--open');
-        modal_forgot.classList.add('modal--open');
-        document.body.classList.add('no-scroll');
-      });
-    } // open modal check
-
-
-    for (var _i6 = 0; _i6 < open_check.length; _i6++) {
-      open_check[_i6].addEventListener('click', function (e) {
-        e.preventDefault();
-        modal_forgot.classList.remove('modal--open');
-        modal_check.classList.add('modal--open');
-        document.body.classList.add('no-scroll');
-      });
-    } // open modal reset
-
-
-    for (var _i7 = 0; _i7 < open_reset.length; _i7++) {
-      open_reset[_i7].addEventListener('click', function (e) {
-        e.preventDefault();
-        modal_check.classList.remove('modal--open');
-        modal_reset.classList.add('modal--open');
-        document.body.classList.add('no-scroll');
-      });
-    } // open modal change pass success
-
-
-    for (var _i8 = 0; _i8 < open_change_success.length; _i8++) {
-      open_change_success[_i8].addEventListener('click', function (e) {
-        e.preventDefault();
-        modal_reset.classList.remove('modal--open');
-        modal_change_success.classList.add('modal--open');
-        document.body.classList.add('no-scroll'); // setTimeout(() => {
-        //   modal_change_success.classList.remove('modal--open');
-        // }, 3000);
-      });
-    } // close all modals
-
-
-    for (var _i9 = 0; _i9 < modal_close.length; _i9++) {
-      modal_close[_i9].addEventListener('click', function (e) {
-        for (var _i10 = 0; _i10 < modals.length; _i10++) {
-          modals[_i10].classList.remove('modal--open');
-        }
-
-        document.body.classList.remove('no-scroll');
-      });
-    }
+      if (!wishlist_wrap.contains(e.target)) {
+        wishlist.classList.remove('headerDropdown--open');
+      }
+    });
   }
 };
